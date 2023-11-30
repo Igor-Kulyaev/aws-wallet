d457abf1aaab38e186d09e418bdf4055545f09e2
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-11-30 17:23:54 +0200

    Add authorizer for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Authenticator} from "./Authenticator";
+import {Button} from "@mui/material";
+import {useAuthenticator} from "@aws-amplify/ui-react";
+
 
 function App() {
+  const {user, signOut} = useAuthenticator((context) => [context.user]);
+  console.log('user', user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      Test
+      <Button variant="contained" color="primary" onClick={signOut}>
+        Logout
+      </Button>
+    </Authenticator>
   );
 }
 
---
+++ client/src/Authenticator.tsx
@@ -0,0 +1,31 @@
+import {Authenticator as AmplifyAuthenticator} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+// @ts-ignore
+export const Authenticator = ({children}) => {
+ return <AmplifyAuthenticator
+  signUpAttributes={["given_name", "family_name"]}
+  formFields={{
+   signIn: {
+    username: {
+     label: "Email",
+     placeholder: "Enter your email",
+    },
+   },
+   signUp: {
+    username: {
+     label: "Email",
+     placeholder: "Enter your email",
+    },
+    given_name: {
+     label: "First name",
+     placeholder: "Enter your first name",
+    },
+    family_name: {
+     label: "Last name",
+     placeholder: "Enter your last name",
+    },
+   },
+  }}
+ >{children}</AmplifyAuthenticator>
+}
