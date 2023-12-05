af3d4dae672833375caeac3a7f1eb422bcce4df8
Igor Kulyaev <kulyaev.igor@gmail.com>
2023-12-05 15:37:34 +0200

    Create income model

--- server/models/income.ts
+++ server/models/income.ts
@@ -7,3 +7,17 @@ interface IIncome {
   createdAt?: Date;
   updatedAt?: Date;
 }
+
+export interface IBaseIncome {
+  name: string;
+  type: string;
+  amount: number;
+}
+
+export interface IIncomeDB extends IBaseIncome {
+  id: string;
+  walletId: string;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+}
